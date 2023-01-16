e27dedf6aa96252e56f1284a7bf169d24ee3bd0c
ΛRSHΛQ SHΛZLY <mjmarshaq@gmail.com>
2023-01-16 18:12:48 +0530

    Data Tables

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter, Search } from '@syncfusion/ej2-react-grids';
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Customers" />
+      <GridComponent id='gridcomp' dataSource={customersData} allowPaging allowSorting width='auto' toolbar={['Delete' ,'Search']} editSettings={{allowDeleting:true, allowEditing:true}}>
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Sort, Filter, Page, Edit, Selection, Toolbar, Search]} />
+      </GridComponent>
+    </div>
   )
 }
 
